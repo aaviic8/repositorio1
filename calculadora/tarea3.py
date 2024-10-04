c967cac7e26744a4a4cbce8742e52d1c90ae36e8
aaviic8 <zx23student3290@colegiovalledelmiro.es>
2024-10-04 08:50:24 +0200

    añadir funcion dividir

---
+++ calculadora/tarea3.py
@@ -0,0 +1,17 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    return a / b
+
+print(sumar(5, 3))
+print(restar(5, 3))
+print(multiplicar(5, 3))
